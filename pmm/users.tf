9b2f3da259ee1630d776762227db71f7b93b1445
Nikita Beletskii <nikita.beletskii@percona.com>
2022-02-09 17:45:32 +0300

    fix name

--- pmm/users.tf
+++ pmm/users.tf
@@ -11,7 +11,7 @@ data "aws_iam_user" "mykola" {
   user_name = "mykola-cli"
 }
 
-resource "aws_iam_policy_attachment" "nikita" {
+resource "aws_iam_policy_attachment" "administrator_access" {
   name       = "nikita.beletskii"
   users      = [aws_iam_user.nikita.name, data.aws_iam_user.mykola.user_name]
   roles      = ["sso-aws-pmm-admin"]
