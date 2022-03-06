42fea64a76466995658900a52f3972e7811f8b39
Nikita Beletskii <nikita.beletskii@percona.com>
2022-03-06 23:28:23 +0400

    SAAS-727 add aurora instance

--- pmmdemo/aws_aurora_engine_2.tf
+++ pmmdemo/aws_aurora_engine_2.tf
@@ -1,17 +1,17 @@
-resource "aws_db_instance" "pmmdemo_aurora_57" {
-  allocated_storage    = 10
-  engine               = "Aurora"
-  engine_version       = "2"
-  instance_class       = "db.t2.medium"
-  name                 = "pmmdemo"
-  username             = "pmmdemo"
-  password             = random_password.pmmdemo_aurora_57_password.result
-  skip_final_snapshot  = true
+resource "aws_rds_cluster" "pmmdemo_aurora_57" {
+  cluster_identifier  = "aurora-cluster-demo"
+  engine              = "aurora-mysql"
+  engine_version      = "5.7.mysql_aurora.2.10.2"
+  database_name       = "pmmdemo"
+  master_username     = "pmmdemo"
+  master_password     = random_password.pmmdemo_aurora_57_password.result
+  skip_final_snapshot = true
+  apply_immediately   = true
 }
 
 resource "random_password" "pmmdemo_aurora_57_password" {
-  length      = 30
-  special     = true
-  upper       = true
-  number      = true
+  length  = 30
+  special = false
+  upper   = true
+  number  = true
 }
