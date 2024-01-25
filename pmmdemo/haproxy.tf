0061d8b48b1118d218e97aa57e25e15f4eaa3134
Michael Coburn <michael.coburn@percona.com>
2024-01-25 15:16:44 -0600

    Consul_mysql_checks (#116)
    * consul mysql health check and waiter.sh mysql condition
    
    * 16 KB again...
    
    * fix EOF typo
    
    * use the correct socket on mysql hosts
    
    * add consul check mysql user to replicas
    
    * remove trailing period
    
    * was checking on the wrong service name!
    
    * update waiter type to haproxy-exporter
    
    * update names and labels for mysql
    
    * resolve dns on haproxy

--- pmmdemo/haproxy.tf
+++ pmmdemo/haproxy.tf
@@ -12,12 +12,13 @@ module "haproxy" {
     aws_security_group.default_access.id,
   ]
   user_data = templatefile("provision_scripts/haproxy.yml", {
-    name                    = local.haproxy_name
     domain                  = var.pmm_domain
+    environment_name        = local.environment_name
+    fqdn                    = "${local.haproxy_name}.${aws_route53_zone.demo_local.name}"
+    local_domain            = "${local.environment_name}.local"
+    mysql_sysbench_password = random_password.percona_xtradb_cluster_80_sysbench_password.result
+    name                    = local.haproxy_name
     pmm_admin_password      = random_password.pmm_admin_pass.result
     pmm_server_endpoint     = local.pmm_server_endpoint
-    fqdn                    = "${local.haproxy_name}.${aws_route53_zone.demo_local.name}"
-    mysql_sysbench_password = random_password.percona_xtradb_cluster_80_sysbench_password.result
-    environment_name        = local.environment_name
   })
 }
