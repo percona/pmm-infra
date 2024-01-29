f5ffb55183bbd4e6c258795696985e723c5785d3
Michael Coburn <michael.coburn@percona.com>
2024-01-29 12:22:49 -0600

    reduce instance classes and organize a bit better (#118)
    * reduce instance classes and organize a bit better
    
    * rename pg

--- pmmdemo/azure_mysql_80.tf
+++ pmmdemo/azure_mysql_80.tf
@@ -1,5 +1,6 @@
 locals {
-  azure_region = "East US"
+  azure_region        = "East US"
+  azure_mysql_80_name = "azure_mysql_80"
 }
 
 # Import the resource group, as there can only be one and it should not
@@ -11,14 +12,14 @@ data "azurerm_resource_group" "pmmdemo" {
 
 # Create the Azure MySQL "RDS" server
 resource "azurerm_mysql_server" "pmmdemo" {
-  name     = "pmmdemo-azure"
+  name     = local.azure_mysql_80_name
   provider = azurerm.demo
 
   location            = local.azure_region
   resource_group_name = data.azurerm_resource_group.pmmdemo.name
 
   administrator_login          = "pmmdemo"
-  administrator_login_password = random_password.azure_mysql.result
+  administrator_login_password = random_password.azure_mysql_80_password.result
 
   sku_name   = "B_Gen5_1"
   storage_mb = 32768
@@ -61,7 +62,7 @@ resource "azurerm_mysql_firewall_rule" "allow_pmmdemo_server" {
   end_ip_address      = aws_eip.external_ip.public_ip
 }
 
-resource "random_password" "azure_mysql" {
+resource "random_password" "azure_mysql_80_password" {
   length  = 30
   special = true
   upper   = true
