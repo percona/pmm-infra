1592d74bcc7ef964fcf9ba008eed046468bbba47
Michael Coburn <michael@gravis.ca>
2024-04-16 13:18:18 -0600

    first commit :rocket:

---
+++ pmmdemo/valkey.tf
@@ -0,0 +1,32 @@
+locals {
+  valkey_name = "valkey"
+}
+
+module "valkey" {
+  source        = "./modules/ec2"
+  server_name   = local.valkey_name
+  instance_type = "t3a.small"
+  subnet_id     = aws_subnet.pmmdemo_private.id
+  route53_id    = aws_route53_zone.demo_local.id
+  security_groups = [
+    aws_security_group.default_access.id,
+  ]
+  user_data = templatefile("provision_scripts/valkey.yml", {
+    domain                  = var.pmm_domain
+    environment_name        = local.environment_name
+    fqdn                    = "${local.ycsb_name}.${aws_route53_zone.demo_local.name}"
+    local_domain            = "${local.environment_name}.local"
+    name                    = local.valkey_name
+    pmm_admin_password      = random_password.pmm_admin_pass.result
+    pmm_server_endpoint     = local.pmm_server_endpoint
+    valkey_primary_password = random_password.valkey_primary_password.result
+  })
+}
+
+resource "random_password" "valkey_primary_password" {
+  length      = 30
+  min_lower   = 1
+  min_numeric = 1
+  min_special = 1
+  min_upper   = 1
+}
