355e1fb071e6553e1d0cfe3e5a4a45553b66ad95
utdrmac <github@matthewboehm.com>
2025-02-12 10:29:21 -0600

    remove data ami object

--- pmmdemo/modules/ec2/data.tf
+++ pmmdemo/modules/ec2/data.tf
@@ -1,18 +1,3 @@
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-  owners = ["amazon"]
-}
 data "aws_key_pair" "pmm-demo" {
   key_name = "pmm-demo"
 }
